ca320e3b2ef6bccc3320945f300122adf8e25ee9
Victor Alfaro <victor.alfaro@dotcms.com>
2020-05-18 13:51:50 -0600

    18504: Introducing github workflow to migrate from Travis to github action. Simplifying scripts

---
+++ .cicd/discover.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+[[ -s .cicd/seed.source ]] && source .cicd/seed.source
+
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/dotCMS/dot-cicd/master/seed/install-dot-cicd.sh)"
+#TODO: restore to previous
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/dotCMS/dot-cicd/issue-18504-ci-github-actions/seed/install-dot-cicd.sh)"
